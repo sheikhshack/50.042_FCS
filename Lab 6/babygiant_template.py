0f26a658ca1ddc9823c3d813e3c0a2ad8cb1ddc7
sheikhshack <redfreak97@gmail.com>
2020-10-27 03:09:49 +0800

    done with cracking

--- Lab 6/babygiant_template.py
+++ Lab 6/babygiant_template.py
@@ -1,18 +1,55 @@
 #50.042 FCS Lab 6 template
 #Year 2019
 
-import math
+from math import sqrt, floor
 import primes
 
+# Mission, solve discrete log
 
 def baby_step(alpha,beta,p,fname):
-    pass
+    m = floor(sqrt(p-1))
+    with open(fname, 'w') as fptr:
+        for i in range(m):
+            res = (primes.square_multiply(alpha, i, p) * beta) % p
+            if i == m-1:
+                fptr.write(str(res))
+            else:
+                fptr.write(str(res))
+                fptr.write('\n')
     
 def giant_step(alpha,p,fname):
-    pass
+    m = floor(sqrt(p - 1))
+    with open(fname, 'w') as fptr:
+        for i in range(m):
+            res = primes.square_multiply(alpha, m * i, p)
+            if i == m - 1:
+                fptr.write(str(res))
+            else:
+                fptr.write(str(res))
+                fptr.write('\n')
 
 def baby_giant(alpha,beta,p):
-    pass
+    # call each step first
+    m = floor(sqrt(p - 1))
+    baby_step(alpha, beta, p, 'baby.txt')
+    giant_step(alpha, p, 'giant.txt')
+    # open baby.txt, store in set
+    lookup_baby = {}
+    with open('baby.txt', 'r') as f1:
+        index = 0
+        for line in f1:
+            # reversed dict
+            lookup_baby[line] = int(index)
+            index += 1
+
+    with open('giant.txt', 'r', newline='\n') as f2:
+        x_g = 0
+        for candidate in f2:
+            if candidate in lookup_baby:
+                x_b = lookup_baby.get(candidate)
+                return (x_g * m) - x_b
+            else:
+                x_g += 1
 
 if __name__=="__main__":
     """
